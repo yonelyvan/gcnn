a0fac45383e4da3d8d356a5a742d3d9952f91019
yonelyvan <yonelyvan@gmail.com>
2018-09-27 06:37:19 -0500

    t

--- train/cnn.py
+++ train/cnn.py
@@ -12,68 +12,66 @@ from PIL import Image
 from sklearn.cross_validation import train_test_split
 os.chdir("/home/y/Desktop/gcnn/train");
 
-###################################################
-# input image dimensions
-m,n = 20,30
-path2="data";
 
-classes=os.listdir(path2)
-x=[]
-y=[]
-for fol in classes:##para cada clase fol==[A,B..2]
-    imgfiles=os.listdir(path2+'/'+fol);
-    for img in imgfiles:##para cada imagen
-        im=Image.open(path2+'/'+fol+'/'+img);
-        im=im.convert(mode='RGB')
-        imrs=im.resize((m,n))
-        imrs=img_to_array(imrs)/255;
-        imrs=imrs.transpose(2,0,1);
-        imrs=imrs.reshape(3,m,n);
-        x.append(imrs)
-        y.append(fol)
-        
-x=np.array(x);
-y=np.array(y);
+def train():
+	# input image dimensions
+	m,n = 32,32
+	path2="data_num";
 
-nb_classes=len(classes)
-print "numero de clases",nb_classes
-#nb_filters=32 nb_conv=3
-x_train, x_test, y_train, y_test= train_test_split(x,y,test_size=0.2,random_state=4)
+	classes=os.listdir(path2)
+	x=[]
+	y=[]
+	for fol in classes:##para cada clase fol==[A,B..2]
+		imgfiles=os.listdir(path2+'/'+fol);
+		for img in imgfiles:##para cada imagen
+			im=Image.open(path2+'/'+fol+'/'+img);
+			im=im.convert(mode='RGB')
+			imrs=im.resize((m,n))
+			imrs=img_to_array(imrs)/255;
+			imrs=imrs.transpose(2,0,1);
+			imrs=imrs.reshape(3,m,n);
+			x.append(imrs)
+			y.append(fol)
+	x=np.array(x);
+	y=np.array(y);
 
-uniques, id_train=np.unique(y_train,return_inverse=True)
-Y_train=np_utils.to_categorical(id_train,nb_classes)
+	nb_classes=len(classes)
+	print "numero de clases",nb_classes
+	#nb_filters=32 nb_conv=3
+	x_train, x_test, y_train, y_test= train_test_split(x,y,test_size=0.2,random_state=4)
 
-uniques, id_test=np.unique(y_test,return_inverse=True)
-Y_test=np_utils.to_categorical(id_test,nb_classes)
-################################# modelo 1
-model= Sequential()
-model.add(Convolution2D(16,3,3,border_mode='same',input_shape=x_train.shape[1:]))
-model.add(Activation('relu'));
-model.add(Convolution2D(16,3,3));
-model.add(Activation('relu'));
-model.add(MaxPooling2D(pool_size=(2,2)));
-model.add(Dropout(0.5));
-model.add(Flatten());
-model.add(Dense(512));
-model.add(Dropout(0.5));
-model.add(Dense(nb_classes));#fullconected?
-model.add(Activation('softmax'));
-model.compile(loss='categorical_crossentropy',optimizer='adadelta',metrics=['accuracy'])
-#################################
-nb_epoch=50; ##iteraciones
-batch_size=50;
-model.fit(x_train,Y_train,batch_size=batch_size,nb_epoch=nb_epoch,verbose=1,validation_data=(x_test, Y_test))
+	uniques, id_train=np.unique(y_train,return_inverse=True)
+	Y_train=np_utils.to_categorical(id_train,nb_classes)
 
-model_json = model.to_json()
-with open("model.json", "w") as json_file:
-    json_file.write(model_json)
-model.save_weights("model.h5")
+	uniques, id_test=np.unique(y_test,return_inverse=True)
+	Y_test=np_utils.to_categorical(id_test,nb_classes)
+	################################# modelo 1
+	model= Sequential()
+	model.add(Convolution2D(16,3,3,border_mode='same',input_shape=x_train.shape[1:]))
+	model.add(Activation('relu'));
+	model.add(Convolution2D(16,3,3));
+	model.add(Activation('relu'));
+	model.add(MaxPooling2D(pool_size=(2,2)));
+	model.add(Dropout(0.5));
+	model.add(Flatten());
+	model.add(Dense(512));
+	model.add(Dropout(0.5));
+	model.add(Dense(nb_classes));#fullconected?
+	model.add(Activation('softmax'));
+	model.compile(loss='categorical_crossentropy',optimizer='adadelta',metrics=['accuracy'])
+	#################################
+	nb_epoch=20; ##iteraciones
+	batch_size=10;
+	model.fit(x_train,Y_train,batch_size=batch_size,nb_epoch=nb_epoch,verbose=1,validation_data=(x_test, Y_test))
 
-
+	model_json = model.to_json()
+	with open("model.json", "w") as json_file:
+	    json_file.write(model_json)
+	model.save_weights("model.h5")
 
 
 
-
+train();
 
 
 
--- train/cnn_reconocimiento.py
+++ train/cnn_reconocimiento.py
@@ -10,55 +10,63 @@ import numpy as np
 import os
 from PIL import Image
 from sklearn.cross_validation import train_test_split
+print "___________________________________________"
 K.set_image_dim_ordering('th')
 
-def cargar_placa_fragmentada(carpeta):
-	os.chdir(path+"proyecto-IA/temp");
-	path1=carpeta;#carpeta
+path = "/home/y/Desktop/gcnn/train";
+
+vector_test =[]
+vector_result =[]
+
+def cargar_imagenes_prueba(carpeta):
+	path1='/home/y/Desktop/gcnn/train/temp' #path+carpeta;#carpeta
 	# input image dimensions
-	m,n = 20,30
+	m,n = 32,32
 	x=[]
-	#imgfiles=os.listdir(path1);
-	#imgfiles.sort()
 	import natsort
 	imgfiles = natsort.natsorted(os.listdir(path1))
 	#print imgfiles
 	for img in imgfiles:##para cada imagen
-	        im=Image.open(path1+'/'+img);
-	        im=im.convert(mode='RGB')
-	        imrs=im.resize((m,n))
-	        imrs=img_to_array(imrs)/255;
-	        imrs=imrs.transpose(2,0,1);
-	        imrs=imrs.reshape(3,m,n);
-	        x.append(imrs)
+		#print img
+		vector_test.append(img)
+		im=Image.open(path1+'/'+img);
+		im=im.convert(mode='RGB')
+		imrs=im.resize((m,n))
+		imrs=img_to_array(imrs)/255;
+		imrs=imrs.transpose(2,0,1);
+		imrs=imrs.reshape(3,m,n);
+		x.append(imrs)
 	x=np.array(x);
 	return x
 
+
 def predecir(x):
-	os.chdir(path+"/proyecto-IA/temp");
+	os.chdir(path+"/temp");
 	# load json and create model
-	json_file = open('model.json', 'r')
+	json_file = open('/home/y/Desktop/gcnn/train/model.json', 'r')
 	loaded_model_json = json_file.read()
 	json_file.close()
 	loaded_model = model_from_json(loaded_model_json)
 	# load weights into new model
-	loaded_model.load_weights("model.h5")
+	loaded_model.load_weights("/home/y/Desktop/gcnn/train/model.h5")
 	# evaluate loaded model on test data
 	loaded_model.compile(loss='categorical_crossentropy',optimizer='adadelta',metrics=['accuracy'])
-
 	predictions = loaded_model.predict(x)
 	pred = predictions*100
 	#print (pred).astype(int)
-	clases = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','P','Q','R','S','T','U','V','W','X','Y','Z']
+	clases = ['c_0','c_1','c_2','c_3','c_4','c_5','c_6','c_7','c_8','c_9'] ##clases
 	label_placa = ''
 	for i in range(len(pred)):
 		m = max(pred[i])
 		index =np.where(pred[i] >=m)[0][0]
 		if m >50:#si tiene mas de 35% de pertenencia 
-			label_placa+=clases[index]
+			#print "clase de pertenencia:", clases[index]
+			vector_result.append(clases[index])
 	return label_placa
 
 
-#x = cargar_placa_fragmentada("p1")
-#predecir(x)
 
+x = cargar_imagenes_prueba("/temp")
+predecir(x)
+for i in range(len(vector_test)):
+	print "imagen: ",vector_test[i]," -> clasificacion;", vector_result[i] 
