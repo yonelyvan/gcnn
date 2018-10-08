a52614ae311eeb595a929b78a8196e868268225e
yonelyvan <yonelyvan@gmail.com>
2018-10-08 06:39:41 -0500

    s

--- g/agmo.py
+++ g/agmo.py
@@ -2,11 +2,25 @@ import random
 from random import randint
 from copy import deepcopy
 
+w = [1,1]
+
+def fx(v):
+	return 4.0*pow(v[0],2.0) + 4.0*pow(v[1],2.0)
+
+def gx(v):
+	return pow((v[0]-5),2.0) + pow((v[1]-5),2.0)
 ##individuo
 class Individuo:
-	def __init__(self, cro, fitness):
+	def __init__(self, cro):
 		self.cro = cro
-		self.fitness = fitness
+		self.fitness = 0
+		self.val_fx = 0
+		self.val_gx = 0
+	
+	def calcular_fitness(self):
+		self.val_fx = fx(self.cro)
+		self.val_gx = gx(self.cro)
+		self.fitness = w[0]*self.val_fx + w[1]*self.val_gx
 
 	def __repr__(self):
 		return "".join(["Individuo(",str(self.cro), ",", str(self.fitness),")"])
@@ -18,6 +32,9 @@ class Poblacion:
 	
 	def insert(self,individuo):
 		self.P.append(individuo)
+
+	def size(self):
+		return len(self.P)
 
 	def __repr__(self):
 		return str(self.P)
@@ -45,24 +62,6 @@ class Stack:
 def getRandom(li,ls): #double
   	return random.uniform(li,ls)
 
-def fx(I):
-	return 4.0*pow(I.cro[0],2.0) + 4.0*pow(I.cro[1],2.0)
-
-def gx(I):
-	return pow((I.cro[0]-5),2.0) + pow((I.cro[1]-5),2.0)
-'''
-cro = [1,1]
-ind = Individuo(cro,8)
-print ind
-ind.cro[0]=2
-print ind
-print fx(ind)
-print gx(ind)
-'''
-
-w = [1,1]
-#print w
-
 # 0<= x <=5
 # 0<= y <=3
 mix_x = 0
@@ -70,9 +69,6 @@ max_x = 5
 min_y = 0
 max_y = 3
 PROB_MUT = 5 # 0.5-1% [0-100]Probabilidad de Mutacion: 0.05
-
-def get_fitness(I):
-	return w[0]*fx(I) + w[1]*gx(I)
 
 
 def es_valido(I):
@@ -85,7 +81,7 @@ def es_valido(I):
 		I.cro[0] = max_x	
 	if I.cro[1] > max_y:
 		I.cro[1] = max_y
-	I.fitness = w[0]*fx(I) + w[1]*gx(I)
+	I.calcular_fitness()
 	
 	x=I.cro[0]
 	y=I.cro[1]
@@ -100,11 +96,11 @@ def get_poblacion_inicial(tam_poblacion):
 	P = Poblacion([])
 	cont = 0
 	while True:
-		I = Individuo([],0)
+		I = Individuo([])
 		x = getRandom(mix_x, max_x);
 		y = getRandom(min_y, max_y);
 		I.cro = [x,y]
-		I.fitness = w[0]*fx(I) + w[1]*gx(I);
+		I.calcular_fitness()
 		
 		if es_valido(I):
 			P.insert(I)
@@ -116,13 +112,9 @@ def get_poblacion_inicial(tam_poblacion):
 def imprimir_poblacion(P):
 	i=0
 	for I in P.P:
-		print i,") ", "CRO[",I.cro[0],",",I.cro[1],"] ",fx(I)," ",gx(I),"	","	|",I.fitness
+		print i,") ", "CRO[",I.cro[0],",",I.cro[1],"] ",I.val_fx," ",I.val_gx,"	","	|",I.fitness
 		i+=1
 
-'''
-P = get_poblacion_inicial(4)
-imprimir_poblacion(P)
-'''
 
 def ruleta(P):
 	total=0;
@@ -162,6 +154,9 @@ def torneo(P):
 def takeFitness(elem):
     return elem.fitness
 
+def takeVal_fx(elem):
+    return elem.val_fx
+
 
 def seleccion(P):
 	return ruleta(P);
@@ -177,11 +172,11 @@ def cruzamiento_blx(P):
 		k = randint(0,tam_pob-1)
 		B = getRandom(-5.0,1.5) 
 		#cruzar
-		I = Individuo([0,0],0) #--
+		I = Individuo([0,0]) #--
 		#p1 + B (P2 - P1)
 		I.cro[0] = P.P[h].cro[0] + B*( P.P[k].cro[0]-P.P[h].cro[0] );
 		I.cro[1] = P.P[h].cro[1] + B*( P.P[k].cro[1]-P.P[h].cro[1] );
-		I.fitness = w[0]*fx(I) + w[1]*gx(I);
+		I.calcular_fitness()
 		if es_valido(I):
 			hijos.insert(I)
 			cont += 1
@@ -191,13 +186,13 @@ def cruzamiento_blx(P):
 
 
 
-def mutar(P):
+def mutar(P): # --
 	tam_pob =len(P.P)
 	for i in range(tam_pob):
 		pro_mut = randint(0,100)
 		if pro_mut <= PROB_MUT:
 			print "Mutacion I: ", i
-			I = Individuo([],0)
+			I = Individuo([])
 			while True:
 				I =deepcopy(P.P[i])
 				k = randint(0,1)
@@ -208,29 +203,27 @@ def mutar(P):
 				if es_valido(I):
 					break
 			P.P[i].cro = I.cro
-			P.P[i].fitness = get_fitness(P.P[i])
-
+			P.P[i].calcular_fitness()
+	return P
 
 
 #retorna la primera frontera y el resto
 def get_frontier(P):
 	p_frontera = Poblacion([]) #fontera
 	P_resto = Poblacion([]) #no estan en la frontera
-	#sort(P.begin(), P.end(), decresiente);#ordenar soluciones en x o fx
-	P.P.sort(key=takeFitness, reverse=True)
+	
+	P.P.sort(key=takeVal_fx, reverse=True)
 
 	front = Stack();
 	front.push(P.P[0]);
 	for i in range(1,len(P.P)):
-		print ":::::::::::: ",i
 		while not front.isEmpty():
-			top_x = fx( front.top() )
-			top_y = gx( front.top() )
-			x = fx( P.P[i]);
-			y = gx( P.P[i]);
+			top_x = front.top().val_fx
+			top_y = front.top().val_gx
+			x = P.P[i].val_fx
+			y = P.P[i].val_gx
 			if top_x >= x  and  top_y >= y:
-				print top_x,">=",x,"	",top_y,">=",y, front.size(),i;
-				print "enter"	
+				#print top_x,">=",x,"	",top_y,">=",y, front.size(),i;
 				P_resto.insert(front.top())   	
 				front.pop()
 			else:
@@ -248,8 +241,9 @@ def get_frontier(P):
 	fronteras.append( P_resto );
 	return fronteras;
 
+
 def get_fronteras(P):
-	contenedor=deepcopy(P);
+	contenedor = deepcopy(P);
 	fronteras = [];#vector de fronteras
 	while contenedor.size()>0:
 		#r = [pobl,pobl,pobl] #ronteras
@@ -258,14 +252,70 @@ def get_fronteras(P):
 		contenedor = r[1];#trabajar con e resto
 	return fronteras;
 
+def distancia(I1, I2):
+	dx = abs(I1.val_fx - I2.val_fx);#x
+	dy = abs(I1.val_gx - I1.val_gx);#y
+	return 2*dx + 2*dy;
+
+def run():
+	iteraciones = 50
+	tam_poblacion = 20
+	D = 4.0
+	P = get_poblacion_inicial(tam_poblacion)
+	for i in range(iteraciones):
+		print ">>>>>>> Iteracion: ",i
+		print "Poblacion"
+		imprimir_poblacion(P)
+
+		print "Seleccion"
+		seleccionados = seleccion(P)
+
+		print "Hijos"
+		hijos = cruzamiento_blx(seleccionados)
+		imprimir_poblacion(hijos)
+
+		for e in hijos.P:
+			P.insert(e)
+		P=mutar(P)
+
+		print "Nueva Poblacion"
+		new_P = Poblacion([])
+		fronteras = get_fronteras(P)
+
+		#nueva poblacion
+		cont=0
+		fl=1
+		index_f=0
+		while fl:
+			for F in fronteras:#F es una poblacion
+				index_f +=1
+				for i in range(F.size()):
+					if i==0 or i == F.size()-1:
+						new_P.insert(F.P[i]);
+						cont +=1
+					else:
+						if distancia(F.P[i-1],F.P[i+1]) >= D:
+							new_P.insert(F.P[i])
+							cont+=1
+					if cont == tam_poblacion:
+						fl=0
+						break
+				if cont == tam_poblacion:
+						fl=0
+						break
+		P = new_P
+		imprimir_poblacion(P)
 
 
 
 
 
+run()
 
 
-def main():
+
+
+def test():
 	P = get_poblacion_inicial(10)
 	imprimir_poblacion(P)
 	
@@ -285,6 +335,9 @@ def main():
 	print "=============frontera 2=========="
 	imprimir_poblacion(R[1])
 	print "======================="
-	
-
-main()
+	'''
+	FF = get_fronteras(hijos)
+	for p in FF:
+		print "frontera"
+		imprimir_poblacion(p)
+	'''	
